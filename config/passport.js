525fc42c2dc128a1e4748aac396c305af74b0c1c
Rahul Chaudhary <rchaudhary@VELOCIS.IN>
2019-07-17 19:15:18 +0530

    a

--- config/passport.js
+++ config/passport.js
@@ -28,12 +28,39 @@ module.exports=function(passport){
 				}else if(rows.length){
 					return done(null,false,req.flash('signupMessage','Username already exists, Please try another username.'));
 				}else{
-					var 
+					var newUserMysql= {
+						username:username,
+						password:bcrypt.hashSync(password,null)
+					};
+
+					var insertQuery="INSERT INTO users (username,password) values (?,?)";
+					connection.query(insertQuery,[newUserMysql.username,newUserMysql.password],
+						function(err,rows){
+							newUserMysql.id=rows.insertId;
+							return done(null,newUserMysql);
+						});
 				}
 			});
-		}
+		});
 
-	
-	)
+	passport.use(
+		'local-login',new LocalStrategy({
+			usernameField:"username",
+			passwordField:"password",
+			passReqToCallback:true
+		},
+		function(req,username,password,done){
+			connection.query("SELECT * FROM users WHERE username=?",[username],function(err,rows){
+					if(err)
+						return done(err);
+					if(!rows.length){
+						return done(null,false,req.flash("loginMessage","No user found!!."));
+					}
+					if(!bcrypt.compareSync(password,rows[0].password))
+						return done(null,rows[0]);
+				});
+		})
 
-}
+		);
+
+};
